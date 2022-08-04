47cb697189556f526a5bb66608212aa626fbb14e
Raccoon <zee_lin@foxmail.com>
2022-08-04 23:00:56 +0800

    新增MongoDB文档数据库，实现浏览记录的增删查操作

---
+++ src/main/java/com/example/demo/document/MemberReadHistory.java
@@ -0,0 +1,32 @@
+package com.example.demo.document;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * 会员商品浏览历史记录
+ * Created by macro on 2018/8/3.
+ */
+@Getter
+@Setter
+@Document
+public class MemberReadHistory {
+    @Id
+    private String id;
+    @Indexed
+    private Long memberId;
+    private String memberNickname;
+    private String memberIcon;
+    @Indexed
+    private Long productId;
+    private String productName;
+    private String productPic;
+    private String productSubTitle;
+    private String productPrice;
+    private Date createTime;
+}
