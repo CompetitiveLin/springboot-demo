d0850f54212a6bd51c78e5a4d752294eb29b1e74
NUC11 <zee_lin@foxmail.com>
2022-11-14 13:20:21 +0800

    Update BinarySearchTest.java
    寻找左侧边界的意思是求 target 在一个含重复数字的升序数组中的最左侧索引值。
    寻找右侧边界的意思是求 target 在一个含重复数字的升序数组中的最右侧索引值。
    如果 target 并不在数组中，则返回-1

--- src/test/java/com/example/demo/Algorithm/BinarySearchTest.java
+++ src/test/java/com/example/demo/Algorithm/BinarySearchTest.java
@@ -1,17 +1,19 @@
 package com.example.demo.Algorithm;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BinarySearchTest {
 
     public static void main(String[] args){
         int[] array1 = {0, 2, 3, 4, 5, 8, 19, 22};
+        System.out.println("Array1： " + Arrays.toString(array1));
         System.out.println(binarySearch(array1, 0, array1.length - 1, 19));
-
-        int[] array2 = {2, 3, 3, 3, 5, 7, 8, 9};
-        System.out.println("Lower bound of array2: " + lower_bound(array2, 0, array1.length, 3));
-        System.out.println("Upper bound of array2: " + upper_bound(array2, 0, array1.length, 3));
-
-        System.out.println("When not found, lower bound of array2: " + lower_bound(array2, 0, array1.length, Integer.MAX_VALUE));
-        System.out.println("When not found, upper bound of array2: " + upper_bound(array2, 0, array1.length, Integer.MIN_VALUE));
+        System.out.println("-----");
+        int[] array2 = {2, 3, 3, 3, 3, 3, 5, 7, 8, 9};
+        System.out.println("Array2: " + Arrays.toString(array2));
+        System.out.println("Lower bound of array2: " + lower_bound(array2, 0, array2.length, 3));
+        System.out.println("Upper bound of array2: " + upper_bound(array2, 0, array2.length, 3));
     }
 
     /**
@@ -50,11 +52,10 @@ public class BinarySearchTest {
                 left = mid + 1;
             } else right = mid;
         }
-
+        if (left >= arrays.length || arrays[left] != target) {
+            return -1;
+        }
          return left;
-
-//        if (left == arrays.length) return -1;
-//        return arrays[left] == target ? left : -1;
     }
 
 
@@ -73,11 +74,10 @@ public class BinarySearchTest {
                 right = mid;
             } else left = mid + 1;
         }
-
+        if (left - 1 < 0 || arrays[left - 1] != target) {
+            return -1;
+        }
          return left - 1;
-
-//        if (left - 1 < 0) return -1;
-//        return arrays[left - 1] == target ? (left - 1) : -1;
     }
 
 
