e9dff2e8711bc61dc1b58eb0182cc1150b5b9f80
NUC11 <zee_lin@foxmail.com>
2023-05-17 20:17:53 +0800

    fix: authentication

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -128,7 +128,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
 //                    .antMatchers("/**")   //测试时全部允许访问
 //                    .permitAll()
-//                    .anyRequest().authenticated()  // 除上面外的所有请求全部需要鉴权认证
+                    .anyRequest().authenticated()  // 除上面外的所有请求全部需要鉴权认证
                     .and()
                 .formLogin()
                     .loginPage("/login.html")
