162394c71a9980749ebaa5b5bcede7e8f5adf8c8
CompetitiveLin <zee_lin@foxmail.com>
2023-08-24 21:36:34 +0800

    feat: long url convert to short url

--- src/main/java/com/example/demo/service/impl/UrlServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UrlServiceImpl.java
@@ -1,19 +1,24 @@
 package com.example.demo.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.example.demo.mbg.mapper.UrlMapper;
 import com.example.demo.mbg.model.Url;
-import com.example.demo.mbg.model.UserInfo;
 import com.example.demo.service.RedisService;
 import com.example.demo.service.UrlService;
 import com.example.demo.util.HashUtil;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
 import static com.example.demo.constant.RedisKeyConstant.url.SHORT_URL_PREFIX;
+import static com.example.demo.constant.RedissonConstant.VIEW_INCREASING_KEY;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +29,8 @@ public class UrlServiceImpl implements UrlService {
 
     private final RedisService redisService;
 
+    private final RedissonClient redissonClient;
+
     private final RBloomFilter<Object> bloomFilter;
 
     //自定义长链接防重复字符串
@@ -33,22 +40,38 @@ public class UrlServiceImpl implements UrlService {
     private static final long TIMEOUT = 24 * 60 * 60;
 
 
+    @Transactional
+    public void increaseViews(String shortUrl) {
+        RLock rLock = redissonClient.getLock(VIEW_INCREASING_KEY);
+        rLock.lock();
+        UpdateWrapper<Url> updateWrapper = Wrappers.update();
+        updateWrapper.lambda().eq(Url::getSurl, shortUrl);
+        updateWrapper.setSql("views = views + 1");
+        urlMapper.update(null, updateWrapper);
+        rLock.unlock();
+    }
+
+
     @Override
+    @Transactional
     public String getLongUrl(String shortUrl) {
         String key = SHORT_URL_PREFIX + shortUrl;
         String longUrl = (String) redisService.get(key);
         if (longUrl != null) {
             redisService.expire(key, TIMEOUT);
+            increaseViews(shortUrl);
             return longUrl;
         }
         QueryWrapper<Url> queryWrapper = new QueryWrapper<>();
         queryWrapper.lambda().eq(Url::getSurl, shortUrl);
         Url url = urlMapper.selectOne(queryWrapper);
         if (url != null) {
-            redisService.set(key, longUrl, TIMEOUT);
             longUrl = url.getLurl();
+            redisService.set(key, longUrl, TIMEOUT);
+            increaseViews(shortUrl);
+            return longUrl;
         }
-        return longUrl;
+        return null;
     }
 
     @Override
